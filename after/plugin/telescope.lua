628633df3c2b71abde7fb0df07e4bbcfc6779070
Philip Bradley <pdbradley@gmail.com>
2023-05-10 04:42:20 -0700

    disable ctrl f find files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -46,7 +46,7 @@ local function telescope_ctrl_p()
 end
 
 
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+--vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
